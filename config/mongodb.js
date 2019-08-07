38a29202d0b6d15b3ec40d7ad7b03c618ca541f5
André Dantas <43660680+andre-haniak@users.noreply.github.com>
2019-08-07 20:10:56 -0300

    Delete mongodb.js
    Removendo credenciais do banco.

--- config/mongodb.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://knowledge:knowledge@knowledge-stats-jl37c.mongodb.net/test?retryWrites=true', {
-        useNewUrlParser: true
-    })
-    .catch(e => {
-        const msg = 'Não foi possível conectar com o MongoDB!'
-        console.log('\x1b[41m%s\x1b[37m', msg, '\x1b[0m')
-    })
